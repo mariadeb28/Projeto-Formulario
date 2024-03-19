270b970f0c64c766b5d081720aba7ffe9f9ad918
mariadeb <deborahgsilva@hotmail.com>
2024-03-18 22:17:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const book = document.querySelector(".botao-book")
+
+
+
+function exibirLinkdosLivros(){
+    
+    const linkbook = "https://www.baixelivros.com.br/dominio-publico"
+    window.open(linkbook, '_blank')
+    
+
+}
+
+console.log(exibirLinkdosLivros)
